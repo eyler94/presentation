8b539fc3ff2d45c8adcbd4afad04b3e205172467
Michael Eyler <eyler94@gmail.com>
2019-06-05 14:50:57 -0600

    Unittest module is ready (basically.)

--- python_practice/dice.py
+++ python_practice/dice.py
@@ -2,13 +2,16 @@ from numpy import random
 
 def roll(num_sides):
     try:
-        num_sides = int(num_sides)
+        num_sides_int = int(num_sides)
     except ValueError:
         return 'Please enter an integer.'
-    if num_sides <= 0:
+    if num_sides_int <= 0:
         return 'Please enter an integer greater than 0.'
-    result = random.randint(1,num_sides+1)
-    return result
+    elif num_sides_int != float(num_sides):
+        return 'Please enter an integer.'
+    else:
+        result = random.randint(1,num_sides_int+1)
+        return result
 
 if __name__ == '__main__':
     result = roll(input("Enter the number of sides: "))
