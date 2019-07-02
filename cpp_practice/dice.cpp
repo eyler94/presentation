9b9008216737441d92a8157983217b34ec0541f3
Michael Eyler <eyler94@gmail.com>
2019-07-02 10:26:56 -0600

    Cpp code seems finished. Now to write the tests.... whoops...

--- cpp_practice/dice.cpp
+++ cpp_practice/dice.cpp
@@ -1,18 +1,28 @@
 // dice.cpp
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 
-int roll(int sides) {
+
+int roll(double sides) {
   if(std::cin.fail()) {
-    std::cout << "Failed.";
-    return "Please enter an integer.";
+    std::cout << "Please enter an integer.\n";
   }
-
-  return sides;
+  else if(sides != int(sides)){
+    std::cout << "Please enter an integer.\n";
+  }
+  else if(sides <= 0){
+    std::cout << "Please enter an integer greater than 0.\n";
+  }
+  else {
+    srand(time(NULL));
+    std::cout << rand()%int(sides) + 1 << "\n";
+  }
 }
 
 int main() {
   std::cout << "Please enter the number of sides: ";
-  int sides;
+  double sides;
   std::cin >> sides;
-  std::cout << roll(sides) << "\n";
+  roll(sides);
 }
